4b20fa16c744df8b0f5f8916b301cb23d5207d00
Jasonvooo <jasonvo1997@gmail.com>
2016-07-05 17:27:22 +1000

    load data

---
+++ Location/SavedData.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+class SavedData: UIViewController{
+    
+    /*
+     func UIColorFromHex(rgbValue:UInt32, alpha:Double=1.0)->UIColor {
+     let red = CGFloat((rgbValue & 0xFF0000) >> 16)/256.0
+     let green = CGFloat((rgbValue & 0xFF00) >> 8)/256.0
+     let blue = CGFloat(rgbValue & 0xFF)/256.0
+     
+     return UIColor(red:red, green:green, blue:blue, alpha:CGFloat(alpha))
+     }
+     
+     func createButton(width:CGFloat,height:CGFloat,x:CGFloat,y:CGFloat,title:String,colour:UInt32,radius:CGFloat)-> UIButton{
+     let button = UIButton(frame:CGRectMake(width, height, x, y))
+     button.backgroundColor = UIColorFromHex(colour)
+     button.setTitle(title, forState: UIControlState.Normal)
+     button.layer.cornerRadius = radius
+     return button
+     }
+     */
+
+    
+}
